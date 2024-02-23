54c4d9eff1fc79fa1cd310008a98d171255a4f79
AnnaSargsyanM <134032907+AnnaSargsyanM@users.noreply.github.com>
2024-02-23 11:00:45 -0600

    Update graviForceFun.cpp

--- graviForceFun.cpp
+++ graviForceFun.cpp
@@ -1,16 +1,3 @@
-//****************************************************************************************************
-//     File:               graviForceFun.cpp
-// 
-//     Student:            Anna Sargsyan
-// 
-//     Assignment:         Program #10
-// 
-//     Course Name:        Programming I
-// 
-//     Course Number:      COSC 1550-01
-// 
-//     Due:                April 7, 2023
-//
 //     This program asks user to input masses of two objects and their 
 //     distance. The program then calculates the gravitational force
 //     and outputs the result.
